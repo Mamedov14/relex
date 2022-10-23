8da2434298161b0f69b2c7982df808745fc11353
Mamedov <vagif_m@bk.ru>
2022-10-23 16:15:39 +0300

    init commit

---
+++ src/test/java/ru/java/relex/RelexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.java.relex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
